6abd97943696447974f4fd7ff5513985267b417b
let1s3n <letisendev@gmail.com>
2023-04-23 19:12:29 -0500

    fix

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,32 @@
-import Head from 'next/head'
+import Head from "next/head";
 /* import Image from 'next/image' */
-import { Container,Row, Col, Image } from 'react-bootstrap'
+import { Container, Row, Col, Image } from "react-bootstrap";
 export default function Home() {
-
   return (
     <>
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@400;700&display=swap" rel="stylesheet" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
         <title>Minera Mirasol</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"
+        />
         <meta name="title" content="Minera Mirasol" />
-        <meta name="description" content="Peruanos en busca de la excelencia haciendo minería responsable." />
-        <meta name="keywords" content="minera, minería, minera mirasol, minería responsable, minería peruana" />
+        <meta
+          name="description"
+          content="Peruanos en busca de la excelencia haciendo minería responsable."
+        />
+        <meta
+          name="keywords"
+          content="minera, minería, minera mirasol, minería responsable, minería peruana"
+        />
       </Head>
-      <Container fluid>
+      <Container fluid className="main-container g-0">
         <Row className="justify-content-center mb-3">
           <Col xs="auto">
             <Image className="logo" src="/images/logo.jpeg" />
@@ -26,24 +37,24 @@ export default function Home() {
             <h1 className="m-0 text-center">En construcción</h1>
           </Col>
         </Row>
-        <Row className="mt-5 g-0 justify-content-around" style={{rowGap:2+'rem'}}>
+        <Row
+          className="g-0 justify-content-around"
+          style={{ rowGap: 2 + "rem",marginTop:"10vw" }}
+        >
           <Col xl={4}>
-            <p className="m-0 fs-6 fs-sm-5 fs-xl-3">
-              Av. Circunvalación Del Golf Los Inkas 134
-              Oficina 707 Torre 1 - Santiago De Surco
-              LIMA - PERÚ
+            <p className="m-0 fs-6 fs-sm-5 text-center">
+              Av. Circunvalación Del Golf Los Inkas 134 Oficina 707 Torre 1 -
+              Santiago De Surco LIMA - PERÚ
             </p>
           </Col>
           <Col xl={4}>
-            <p className="m-0 fs-6 fs-sm-5 fs-xl-3">
-              Planta Metalúrgica MIRASOL <br /> Autopista Panamericana Norte KM 332 Culebras, Huarmey, Ancash, Perú
+            <p className="m-0 fs-6 fs-sm-5 text-center">
+              Planta Metalúrgica MIRASOL <br /> Autopista Panamericana Norte KM
+              333 Culebras, Huarmey, Ancash, Perú
             </p>
           </Col>
         </Row>
       </Container>
-
-
-
     </>
-  )
+  );
 }
